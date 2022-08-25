17a5cf6b1abaf055dea4cbc607434f6492697c57
Mohanad hisham <moh.shadouf1@gmail.com>
2022-08-25 18:01:08 +0300

    updated

--- main.dart
+++ main.dart
@@ -1,4 +1,14 @@
 void main() {
-  String myname = "mohanad";
-  print(myname);
+  // ignore: unused_local_variable
+  Map mohanadInformation = {
+    "first_name": "mohanad",
+    "last_name": "aboshadouf",
+    "age": 22,
+    "educatio info": {
+      "name": "zarqa university",
+      "faculity": "IT",
+      "year": 2022
+    }
+  };
+  print(mohanadInformation);
 }
